9ae21d2fe73bd31dafc03e9a62d58d14ec361c93
thinkaw <673761415@qq.com>
2022-07-24 14:50:36 +0800

    add flask test:

--- test.py
+++ test.py
@@ -22,8 +22,8 @@
 # print(block.hash)
 
 from web3 import Web3
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/352b1dd7e7934d9cb476cecc9691c5bc'))
-
+# w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/352b1dd7e7934d9cb476cecc9691c5bc'))
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 block = w3.eth.getBlock(12345)
 # 输出区块信息
 print(block)
